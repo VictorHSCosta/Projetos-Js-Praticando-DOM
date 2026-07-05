74f04ac79a3311792698737571571b37ae2056af
VictorHSCosta <victorhenriquecosta23@gmail.com>
2026-07-04 22:23:46 -0300

    feat: Implement calculator functionality with button event listeners and display updates

---
+++ src/pages/calculadora/index.js
@@ -0,0 +1,43 @@
+const allElements = Array.from(document.getElementById("elements-box").children)
+  .map((value) => value.id)
+  .filter(Boolean);
+
+
+const screen = document.getElementById("display");
+
+// we need to add a listener on all buttons 
+
+allElements.forEach((value) => {
+  const element = document.getElementById(value);
+
+  element.addEventListener("click", clickOnButton);
+});
+
+
+function clickOnButton(event){
+  //filtra comandos especiais como c e =
+  switch(event.target.innerText.toString()){
+    case "C":
+      clearScreen(screen);
+      return;
+      break;
+    default:
+      break;
+  }
+
+  let valueOfScreen = screen.innerText.toString();
+
+  screen.innerText = extractTextFromEvent(valueOfScreen, event);
+}
+
+function extractTextFromEvent(value, event){
+  if (value == "0"){
+    return event.target.innerText.toString();
+  }
+
+  return value + event.target.innerText.toString();
+}
+
+function clearScreen(screen) {
+  screen.innerText = "0";
+}
